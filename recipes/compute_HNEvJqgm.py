a456d42ec3edb230a87176b8d37858a8750ee9ef
ajelley <ajelley>
2019-10-25 15:49:24 +0200

    Saved recipe: DATAIKUOCRDEMO.compute_HNEvJqgm

--- recipes/compute_HNEvJqgm.py
+++ recipes/compute_HNEvJqgm.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+pdf_Input = dataiku.Folder("QZb3pfTL")
+pdf_Input_info = pdf_Input.get_info()
+
+
+
+
+# Write recipe outputs
+pdf_Images = dataiku.Folder("HNEvJqgm")
+pdf_Images_info = pdf_Images.get_info()
+
+
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
