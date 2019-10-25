5e76e0ebbc74b97f41cfb59a01536dc0aae939c2
ajelley <ajelley>
2019-10-25 16:25:54 +0200

    Saved recipe: DATAIKUOCRDEMO.compute_PDF_text

--- recipes/compute_PDF_text.py
+++ recipes/compute_PDF_text.py
@@ -6,11 +6,14 @@ from dataiku import pandasutils as pdu
 # Read recipe inputs
 pdfs_Processed = dataiku.Folder("w3DdXIhY")
 pdfs_Processed_info = pdfs_Processed.get_info()
-
+pdfs_Processed_path = pdfs_Processed.get_path()
 
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+
+PDF_pages = sorted(os.listdir(pdfs_Processed_path))
+PDF_pages_path = [os.path.join(images_path, page) for page in PDF_pages]
 
 pdf_text_df = ... # Compute a Pandas dataframe to write into PDF_text
 
