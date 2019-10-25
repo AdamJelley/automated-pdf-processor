33e449bfd0b0d2b202c10c8fd9423aa6f642ee1b
ajelley <ajelley>
2019-10-25 15:10:14 +0200

    Saved recipe: DATAIKUOCRDEMO.compute_wMThJzRU

--- recipes/compute_wMThJzRU.py
+++ recipes/compute_wMThJzRU.py
@@ -11,40 +11,12 @@ pdf_Input = dataiku.Folder("NlFjvamp")
 pdf_Input_info = pdf_Input.get_info()
 input_path = pdf_Input.get_path()
 
-examplePDF_path = os.path.join(input_path, 'Dataiku.pdf')
+examplePDF_path = os.path.join(input_path, 'Dataiku_Overview.pdf')
 
 # Write recipe outputs
 pdf_Images = dataiku.Folder("wMThJzRU")
 pdf_Images_info = pdf_Images.get_info()
-
-
-
+pdf_Images_path = pdf_Images.get_path()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Read recipe inputs
-pdfs = dataiku.Folder("Sz02XquZ")
-pdfs_info = pdfs.get_info()
-
-
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
-
-input_path = pdfs.get_path()
-
-examplePDF_path = os.path.join(input_path, 'PlusnetBillJuly.pdf')
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-print("Path = " + examplePDF_path)
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Write recipe outputs
-#pdf_processed = dataiku.Dataset("PDF_processed")
-#pdf_processed.write_with_schema(pdf_processed_df)
-
-images = dataiku.Folder("fWe0pI5t")
-images_info = images.get_info()
-images_path = images.get_path()
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-examplePDF_image = convert_from_path(examplePDF_path,fmt='jpg',output_folder=images_path)
+examplePDF_image = convert_from_path(examplePDF_path,fmt='jpg',output_folder=pdf_Images_path)
