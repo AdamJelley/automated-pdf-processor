3c9a6bbc82d25e395aaae1390de78c1d6074b6de
ajelley <ajelley>
2019-10-28 19:40:58 +0100

    Recipe DATAIKUOCRDEMO.compute_1GEMH8ef saved through notebook notebook editor for compute_1GEMH8ef

--- recipes/compute_1GEMH8ef.py
+++ recipes/compute_1GEMH8ef.py
@@ -1,15 +1,23 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+import os
 
 # Read recipe inputs
 pdf_topics = dataiku.Dataset("PDF_topics")
 pdf_topics_df = pdf_topics.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+outputText = pdf_topics_df['text_summary'][0]
 
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 pdf_topics_text = dataiku.Folder("1GEMH8ef")
 pdf_topics_text_info = pdf_topics_text.get_info()
+pdf_topics_text.get_path()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+with open(os.path.join('/path/to/Documents',completeName)"PDF_summary.txt", "w") as text_file:
+    text_file.write(outputText)
