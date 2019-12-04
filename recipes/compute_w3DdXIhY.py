6693028fddd46d4ed1b684664b0fb4423dfa0c20
ajelley <ajelley>
2019-12-04 20:24:57 +0100

    Recipe DATAIKUOCRDEMO.compute_w3DdXIhY saved through notebook notebook editor for compute_w3DdXIhY

--- recipes/compute_w3DdXIhY.py
+++ recipes/compute_w3DdXIhY.py
@@ -20,20 +20,22 @@ pdfs_processed_info = pdfs_processed.get_info()
 pdfs_folder_path = pdfs_processed.get_path()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+os.listdir(pdfs_folder_path)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 for output_pdf_folder in os.listdir(pdfs_folder_path):
-    os.rmdir(output_pdf_folder)
+    os.rmdir(os.path.join(pdfs_folder_path, output_pdf_folder))
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 for pdf_folder in os.listdir(images_folder_path):
-    print(pdf_folder)
     PDF_pages = sorted(os.listdir(os.path.join(images_folder_path, pdf_folder)))
     PDF_pages_path = [os.path.join(images_folder_path, pdf_folder) for pdf_folder in PDF_pages]
 
     pdfs_processed_path = os.path.join(pdfs_folder_path, pdf_folder)
     os.mkdir(pdfs_processed_path)
 
-    for i, page_path in enumerate(PDF_pages_path):
-
+    for i, page_path in enumerate(os.listdir(pdfs_processed_path)):
+        print(PDF_pages_path)
         output_path = os.path.join(pdfs_processed_path, 'page_'+"{0:0=2d}".format(i+1)+'.txt')
         pdf_text = pytesseract.image_to_string(Image.open(page_path))
         with open(output_path, "w") as f:
