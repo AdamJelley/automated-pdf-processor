45862bb114136eb4b114d7506dcce2b143badc7d
ajelley <ajelley>
2019-12-04 20:48:47 +0100

    Recipe DATAIKUOCRDEMO.compute_w3DdXIhY saved through notebook notebook editor for compute_w3DdXIhY

--- recipes/compute_w3DdXIhY.py
+++ recipes/compute_w3DdXIhY.py
@@ -20,9 +20,6 @@ pdfs_processed_info = pdfs_processed.get_info()
 pdfs_folder_path = pdfs_processed.get_path()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-os.listdir(pdfs_folder_path)
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 for output_pdf_folder in os.listdir(pdfs_folder_path):
     output_pdf_folder_path = os.path.join(pdfs_folder_path, output_pdf_folder)
     for page in os.listdir(output_pdf_folder_path):
@@ -33,31 +30,15 @@ for output_pdf_folder in os.listdir(pdfs_folder_path):
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 for pdf_folder in os.listdir(images_folder_path):
     pdf_folder_path = os.path.join(images_folder_path, pdf_folder)
-    PDF_pages = sorted(os.listdir(os.path.join(images_folder_path, pdf_folder)))
+    PDF_pages = os.listdir(os.path.join(images_folder_path, pdf_folder))
     PDF_pages_path = [os.path.join(pdf_folder_path, page) for page in os.listdir(pdf_folder_path)]
     pdfs_processed_path = os.path.join(pdfs_folder_path, pdf_folder)
     os.mkdir(pdfs_processed_path)
 
-    for i, page_path in enumerate(PDF_pages_path):
+    for i, page_path in enumerate(sorted(PDF_pages_path)):
         print(page_path)
         output_path = os.path.join(pdfs_processed_path, 'page_'+"{0:0=2d}".format(i+1)+'.txt')
         pdf_text = pytesseract.image_to_string(Image.open(page_path))
         with open(output_path, "w") as f:
             f.write(pdf_text)
             f.close()
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
-
-PDF_pages = sorted(os.listdir(images_path))
-PDF_pages_path = [os.path.join(images_path, page) for page in PDF_pages]
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-for i, page_path in enumerate(PDF_pages_path):
-    output_path = os.path.join(pdfs_processed_path, 'page_'+"{0:0=2d}".format(i+1)+'.txt')
-    pdf_text = pytesseract.image_to_string(Image.open(page_path))
-    with open(output_path, "w") as f:
-        f.write(pdf_text)
-        f.close()
