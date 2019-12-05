11d87a22d23258ff2db6b99d1d3be6d88072eafc
ajelley <ajelley>
2019-12-05 22:57:39 +0100

    Recipe DATAIKUOCRDEMO.compute_PDF_text saved through notebook notebook editor for compute_PDF_text

--- recipes/compute_PDF_text.py
+++ recipes/compute_PDF_text.py
@@ -8,11 +8,13 @@ import re
 import nltk
 #nltk.download('punkt')
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 pdfs_Processed = dataiku.Folder("w3DdXIhY")
 pdfs_Processed_info = pdfs_Processed.get_info()
 pdfs_Processed_path = pdfs_Processed.get_path()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
@@ -21,16 +23,20 @@ PDF_pages = sorted(os.listdir(pdfs_Processed_path))
 PDF_pages_path = [os.path.join(pdfs_Processed_path, page) for page in PDF_pages]
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-allPagestxt = ''
-for pagetxt in PDF_pages_path:
-    allPagestxt += '\n' + open(pagetxt, 'r').read()
+PDF_pages_folders = os.listdir(pdfs_Processed_path)
+PDF_pages__folders_paths = [os.path.join(pdfs_Processed_path, folder) for folder in PDF_pages_folders]
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-allPagestxt_simple = allPagestxt
-allPagestxt_simple = re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r"\1 ", allPagestxt_simple)
+sentencesList = []
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-sentencesList = [allPagestxt_simple]
+for folder_path in PDF_pages__folders_paths:
+    PDF_pages = sorted(os.listdir(folder_path))
+    PDF_pages_path = [os.path.join(folder_path, page) for page in PDF_pages]
+    allPagestxt = ''
+    for pagetxt in PDF_pages_path:
+        allPagestxt += '\n' + open(pagetxt, 'r').read()
+    allPagestxt_simple = re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r"\1 ", allPagestxt)
+    sentencesList.append(allPagestxt_simple)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 #sentencesList = nltk.sent_tokenize(allPagestxt_simple)
