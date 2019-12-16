2cc7030067fbc227f5cac655441a0496c977713e
ajelley <ajelley>
2019-12-16 19:39:11 +0100

    Saved web app InputFolder_iFrame (DATAIKUOCRDEMO.Qd2fGuq)

--- web_apps/Qd2fGuq/app.js
+++ web_apps/Qd2fGuq/app.js
@@ -1,3 +1,1 @@
-var iframe = document.getElementById("iframe");
-var elmnt = iframe.contentWindow.document.querySelector("#root-dom-element > div.row-fluid.master-nav.noflex.ng-scope");
-elmnt.style.display = "none";
+
