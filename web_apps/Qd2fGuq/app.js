6dbe4d56c6eb4fdc5706ce9bba3037e9455c7a3b
ajelley <ajelley>
2019-12-16 19:35:55 +0100

    Saved web app InputFolder_iFrame (DATAIKUOCRDEMO.Qd2fGuq)

--- web_apps/Qd2fGuq/app.js
+++ web_apps/Qd2fGuq/app.js
@@ -1,2 +1,2 @@
-var DSS_header = document.querySelector("#root-dom-element > div.row-fluid.master-nav.noflex.ng-scope"
+var DSS_header = document.querySelector("#root-dom-element > div.row-fluid.master-nav.noflex.ng-scope")
 DSS_header.style.display = "none";
