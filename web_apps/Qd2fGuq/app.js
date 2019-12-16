2ce5f4f41d8d8185eedd112b8b2bfbacb19d86dd
ajelley <ajelley>
2019-12-16 19:52:38 +0100

    Saved web app InputFolder_iFrame (DATAIKUOCRDEMO.Qd2fGuq)

--- web_apps/Qd2fGuq/app.js
+++
@@ -1,3 +0,0 @@
-var iframe = document.getElementById("iframe");
-var elmnt = iframe.contentWindow.document.getElementsBySelector("div.row-fluid.master-nav.noflex.ng-scope")[0];
-elmnt.style.display = "none";
